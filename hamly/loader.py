11118a54f3e150ff3fe1f964c55b6e2368dbf9bf
Victor Kotseruba <barbuzaster@gmail.com>
2013-11-06 00:23:08 +0400

    setup script, astmonkey for source generation, loader cache

--- hamly/loader.py
+++ hamly/loader.py
@@ -6,33 +6,51 @@ from .compiler import compile_tree
 from .optimizer import optimize
 from .const import WRITE, TO_STRING, ESCAPE, WRITE_MULTI, QUOTEATTR, WRITE_ATTRS
 
+cache = {}
 
 def get_template(filename):
-    with open(filename) as fp:
-        source = fp.read()
+    cached = cache.get(filename)
 
-    tree = parse(source)
-    compiled = compile_tree(tree)
-    module = ast.Module(compiled)
-    optimized = optimize(module)
+    if not cached:
+        with open(filename) as fp:
+            source = fp.read()
 
-    optimized.body.insert(0, ast.ImportFrom(module='hamly.escape', names=[ast.alias(name='escape', asname=ESCAPE)], level=0))
-    optimized.body.insert(0, ast.ImportFrom(module='hamly.escape', names=[ast.alias(name='quoteattr', asname=QUOTEATTR)], level=0))
-    optimized.body.insert(0, ast.ImportFrom(module='hamly.escape', names=[ast.alias(name='soft_unicode', asname=TO_STRING)], level=0))
-    optimized.body.insert(0, ast.ImportFrom(module='hamly.html', names=[ast.alias(name='write_attrs', asname=WRITE_ATTRS)], level=0))
+        tree = parse(source)
+        compiled = compile_tree(tree)
+        module = ast.Module(compiled)
+        optimized = optimize(module)
 
-    code = compile(ast.fix_missing_locations(optimized), filename, 'exec')
 
-    scope = {}
-    exec code in scope
-    main_fun = scope["main"]
-    concat = ''.join
+        optimized.body.insert(0, ast.ImportFrom(module='hamly.escape',
+                                                names=[ast.alias(name='escape', asname=ESCAPE),
+                                                       ast.alias(name='quoteattr', asname=QUOTEATTR),
+                                                       ast.alias(name='soft_unicode', asname=TO_STRING)], level=0))
+        optimized.body.insert(0, ast.ImportFrom(module='hamly.html', names=[ast.alias(name='write_attrs', asname=WRITE_ATTRS)], level=0))
 
-    def render(**kwargs):
-        output = []
-        context = {WRITE: output.append, WRITE_MULTI: output.extend}
-        context.update(kwargs)
-        main_fun(**context)
-        return concat(output)
+        template_source = ""
+        try:
+            from astmonkey import visitors
+            template_source = visitors.to_source(optimized)
+        except ImportError:
+            pass
 
-    return render
+        code = compile(ast.fix_missing_locations(optimized), filename, 'exec')
+
+        scope = {}
+        exec code in scope
+        main_fun = scope["main"]
+        concat = ''.join
+
+        def render(**kwargs):
+            output = []
+            context = {WRITE: output.append, WRITE_MULTI: output.extend}
+            context.update(kwargs)
+            main_fun(**context)
+            return concat(output)
+
+        setattr(render, "template_source", template_source)
+
+        cached = render
+        cache[filename] = cached
+
+    return cached
--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, Extension
 
 setup(
     name='hamly',
+    description='fast haml for python',
+    keywords='web template haml',
     version='0.1',
     author='Victor Kotseruba',
     author_email='barbuzaster@gmail.com',
     license='MIT',
     packages=['hamly'],
+    install_requires=['pyparsing==1.5.7', 'pydot', 'astmonkey'],
     include_package_data=True,
     zip_safe=False,
     ext_modules=[Extension('hamly.escape_fast', ['hamly/escape_fast.c'])]
